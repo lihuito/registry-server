dd38c6c9ec57509843852c27c044ad890e2895ec
lihuito@163.com <lihui19811015>
2018-10-30 14:09:09 +0800

    修改信息

---
+++ src/main/java/registryserver/registryServerApplication.java
@@ -0,0 +1,16 @@
+package registryserver;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class registryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(registryServerApplication.class, args);
+	}
+}
